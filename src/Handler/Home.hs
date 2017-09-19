7d311932d38ba8fdbe79124c539428a370c53335
patrick brisbin <pbrisbin@gmail.com>
2017-09-19 12:03:09 -0400

    Remove unused extensions

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -1,8 +1,6 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE TypeFamilies #-}
 module Handler.Home where
 
 import Import
