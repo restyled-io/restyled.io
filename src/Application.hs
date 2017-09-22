7baab9a3764f823243b7e311d8e7db17c88e968a
patrick brisbin <pbrisbin@gmail.com>
2017-09-22 19:51:52 -0400

    Add a tool for replaying webhooks locally

--- src/Application.hs
+++ src/Application.hs
@@ -42,6 +42,7 @@ import Handler.Common
 import Handler.Docs
 import Handler.Home
 import Handler.Signup
+import Handler.Webhooks
 
 -- This line actually creates our YesodDispatch instance. It is the second half
 -- of the call to mkYesodData which occurs in Foundation.hs. Please see the
--- src/Model.hs
+++ src/Model.hs
@@ -2,19 +2,44 @@
 {-# LANGUAGE FlexibleInstances          #-}
 {-# LANGUAGE GADTs                      #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE LambdaCase                 #-}
 {-# LANGUAGE MultiParamTypeClasses      #-}
 {-# LANGUAGE NoImplicitPrelude          #-}
 {-# LANGUAGE OverloadedStrings          #-}
 {-# LANGUAGE TemplateHaskell            #-}
 {-# LANGUAGE TypeFamilies               #-}
-module Model where
+{-# LANGUAGE ConstraintKinds            #-}
+module Model
+    ( module Model
+    , module Model.Base
+    ) where
 
 import ClassyPrelude.Yesod
+
 import Database.Persist.Quasi
 
--- You can define all of your database entities in the entities file.
--- You can find more information on persistent and how to declare entities
--- at:
--- http://www.yesodweb.com/book/persistent/
+import Model.Base
+
+-- | A shorthand Context for working with Entities generically
+--
+-- > whatever :: DB m b r -> _ -> _ -> ReaderT b m (_)
+--
+type DB m backend record =
+    ( MonadIO m
+    , PersistEntity record
+    , PersistEntityBackend record ~ BaseBackend backend
+    , PersistUniqueWrite backend
+    )
+
 share [mkPersist sqlSettings, mkMigrate "migrateAll"]
     $(persistFileWith lowerCaseSettings "config/models")
+
+-- | Yes, we accept the race condition you expect
+findOrCreate :: DB m b r => r -> ReaderT b m (Key r)
+findOrCreate v = getByValue v >>= \case
+    Just (Entity k _) -> k <$ replace k v
+    Nothing -> insert v
+
+-- | A verson of @'findOrCreateEntity'@ that returns a complete @'Entity'@
+findOrCreateEntity :: DB m b r => r -> ReaderT b m (Entity r)
+findOrCreateEntity v = Entity <$> findOrCreate v <*> pure v
