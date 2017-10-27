9c3695ec258dbc2189e81f27c592e391abb54d82
patrick brisbin <pbrisbin@gmail.com>
2017-10-27 16:04:07 -0700

    Simplify URI parsing modules
    Remove duplication by relying more on libraries, rather than attempt to
    extract and share our own versions of (e.g.) note and partial.

--- src/Network/PGDatabaseURL.hs
+++ src/Network/PGDatabaseURL.hs
@@ -2,70 +2,30 @@ module Network.PGDatabaseURL
     ( parsePGConnectionString
     ) where
 
-import Data.Maybe (isJust)
-import Data.String (IsString(..))
+import Control.Error.Util (note)
+import Control.Monad (guard)
+import Control.Monad.Plus (partial)
+import Data.ByteString (ByteString)
 import Network.HTTP.Base
-    ( host
-    , parseURIAuthority
-    , password
-    , port
-    , uriToAuthorityString
-    , user
-    )
 import Network.URI (parseURI, uriPath, uriScheme)
+import qualified Data.ByteString.Char8 as C8
 
-data PGConnection = PGConnection
-    { connUser :: Maybe String
-    , connPassword :: Maybe String
-    , connHost :: Maybe String
-    , connPort :: Maybe Int
-    , connDBname :: Maybe String
-    }
+-- | Parse @postgres://{user}:{password}@{host}:{port}/{db}@
+parsePGConnectionString :: String -> Either String ByteString
+parsePGConnectionString url = do
+    uri <- note "Invalid URI" $ parseURI url
+    note "Wrong scheme" $ guard $ uriScheme uri == "postgres:"
+    uriAuth <- note "Missing or invalid Authority"
+        $ parseURIAuthority
+        $ uriToAuthorityString uri
 
-parsePGConnectionString :: IsString a => String -> Either String a
-parsePGConnectionString = fmap toConnectionString . parsePGDatabaseURL
-
-parsePGDatabaseURL :: String -> Either String PGConnection
-parsePGDatabaseURL url = do
-    uri <- checkP "URI" parseURI url
-    uriAuth <- checkP "Authority" parseURIAuthority $ uriToAuthorityString uri
-    checkScheme $ uriScheme uri
-
-    return PGConnection
-        { connUser = user uriAuth
-        , connPassword = password uriAuth
-        , connHost = emptyToMaybe $ host uriAuth
-        , connPort = port uriAuth
-        , connDBname = emptyToMaybe $ dropChar '/' $ uriPath uri
-        }
-
-toConnectionString :: IsString a => PGConnection -> a
-toConnectionString c = fromString $ unwords
-    -- N.B. Pattern match is safe because of preceding filter
-    $ map (\(k, Just v) -> k ++ "=" ++ v)
-    $ filter (isJust . snd)
-    [ ("user", connUser c)
-    , ("password", connPassword c)
-    , ("host", connHost c)
-    , ("port", show <$> connPort c)
-    , ("dbname", connDBname c)
-    ]
-
-checkScheme :: String -> Either String ()
-checkScheme "postgres:" = return ()
-checkScheme x = Left $
-    "Invalid scheme: " ++ x ++ "//, expecting postgres://"
-
-checkP :: String -> (String -> Maybe a) -> String -> Either String a
-checkP label p x = check ("Invalid " ++ label ++ ": " ++ x) $ p x
-
-check :: String -> Maybe a -> Either String a
-check msg = maybe (Left msg) Right
-
-emptyToMaybe :: [a] -> Maybe [a]
-emptyToMaybe [] = Nothing
-emptyToMaybe x = Just x
-
-dropChar :: Char -> String -> String
-dropChar c (x:xs) | c == x = xs
-dropChar _ x = x
+    return $ C8.pack $ unwords
+        [ k ++ "=" ++ v
+        | (k, Just v) <-
+            [ ("user", user uriAuth)
+            , ("password", password uriAuth)
+            , ("host", partial (not . null) $ host uriAuth)
+            , ("port", show <$> port uriAuth)
+            , ("dbname", partial (not . null) $ dropWhile (== '/') $ uriPath uri)
+            ]
+        ]
--- src/Network/RedisURL.hs
+++ src/Network/RedisURL.hs
@@ -2,55 +2,31 @@ module Network.RedisURL
     ( parseRedisURL
     ) where
 
+import Control.Error.Util (note)
+import Control.Monad (guard)
+import Control.Monad.Plus (partial)
 import Data.Maybe (fromMaybe)
 import Database.Redis
 import Network.HTTP.Base
-    ( host
-    , parseURIAuthority
-    , password
-    , port
-    , uriToAuthorityString
-    )
 import Network.URI (parseURI, uriPath, uriScheme)
 import Text.Read (readMaybe)
-
 import qualified Data.ByteString.Char8 as C8
 
+-- | Parse @redis://{ignored}:{password}@{host}:{port}/{db}@
 parseRedisURL :: String -> Either String ConnectInfo
 parseRedisURL url = do
-    uri <- checkP "URI" parseURI url
-    uriAuth <- checkP "Authority" parseURIAuthority $ uriToAuthorityString uri
-    checkScheme $ uriScheme uri
+    uri <- note "Invalid URI" $ parseURI url
+    note "Wrong scheme" $ guard $ uriScheme uri == "redis:"
+    uriAuth <- note "Missing or invalid Authority"
+        $ parseURIAuthority
+        $ uriToAuthorityString uri
 
     return defaultConnectInfo
         { connectHost = fromMaybe (connectHost defaultConnectInfo)
-            $ emptyToMaybe $ host uriAuth
+            $ partial (not . null) $ host uriAuth
         , connectPort = fromMaybe (connectPort defaultConnectInfo)
             $ PortNumber . fromIntegral <$> port uriAuth
         , connectAuth = C8.pack <$> password uriAuth
         , connectDatabase = fromMaybe (connectDatabase defaultConnectInfo)
-            $ readMaybe =<< emptyToMaybe (dropChar '/' $ uriPath uri)
-
-        -- Left as defaults:
-        -- , connectMaxConnections = 0
-        -- , connectMaxIdleTime = 50
+            $ readMaybe =<< partial (not . null) (dropWhile (== '/') $ uriPath uri)
         }
-
-checkScheme :: String -> Either String ()
-checkScheme "redis:" = return ()
-checkScheme x = Left $
-    "Invalid scheme: " ++ x ++ "//, expecting redis://"
-
-checkP :: String -> (String -> Maybe a) -> String -> Either String a
-checkP label p x = check ("Invalid " ++ label ++ ": " ++ x) $ p x
-
-check :: String -> Maybe a -> Either String a
-check msg = maybe (Left msg) Right
-
-emptyToMaybe :: [a] -> Maybe [a]
-emptyToMaybe [] = Nothing
-emptyToMaybe x = Just x
-
-dropChar :: Char -> String -> String
-dropChar c (x:xs) | c == x = xs
-dropChar _ x = x
