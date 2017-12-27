e1739d2063f862ec8df4037bb5ad47368a32eb4d
patrick brisbin <pbrisbin@gmail.com>
2017-12-27 14:11:24 -0500

    Move Model.Base to well-named module

---
+++ src/GitHub/Instances.hs
@@ -0,0 +1,33 @@
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+module GitHub.Instances where
+
+import Data.Proxy
+import Data.Text (Text)
+import Database.Persist.Sql
+import GitHub.Data (Id, Name, mkId, mkName, untagId, untagName)
+
+instance PersistField (Id a) where
+    toPersistValue = toPersistValue . untagId
+    fromPersistValue = (mkId Proxy <$>) . fromPersistValue
+
+instance PersistFieldSql (Id a) where
+    sqlType _ = sqlType (Proxy :: Proxy Int)
+
+instance PersistField (Name a) where
+    toPersistValue = toPersistValue . untagName
+    fromPersistValue = (mkName Proxy <$>) . fromPersistValue
+
+instance PersistFieldSql (Name a) where
+    sqlType _ = sqlType (Proxy :: Proxy Text)
+
+instance Num (Id a) where
+    fromInteger = mkId Proxy . fromInteger
+
+    (+) = errSneakyNum
+    (-) = errSneakyNum
+    (*) = errSneakyNum
+    abs = errSneakyNum
+    signum = errSneakyNum
+
+errSneakyNum :: a
+errSneakyNum = error "Num instance only present for `fromInteger'"
--- src/Model.hs
+++ src/Model.hs
@@ -1,20 +1,23 @@
-{-# LANGUAGE EmptyDataDecls             #-}
-{-# LANGUAGE FlexibleInstances          #-}
-{-# LANGUAGE GADTs                      #-}
+{-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE EmptyDataDecls #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GADTs #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE LambdaCase                 #-}
-{-# LANGUAGE MultiParamTypeClasses      #-}
-{-# LANGUAGE NoImplicitPrelude          #-}
-{-# LANGUAGE OverloadedStrings          #-}
-{-# LANGUAGE TemplateHaskell            #-}
-{-# LANGUAGE TypeFamilies               #-}
-{-# LANGUAGE ConstraintKinds            #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
 module Model where
 
 import ClassyPrelude.Yesod
 
 import Database.Persist.Quasi
-import Model.Base
+import GitHub.Data (Id, Name, Owner, PullRequest, Repo)
+import GitHub.Data.Apps (Installation)
+import GitHub.Instances ()
 
 share [mkPersist sqlSettings, mkMigrate "migrateAll"]
     $(persistFileWith lowerCaseSettings "config/models")
