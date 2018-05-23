50237d7b21347ef112e68a722ea0a275db9cb33f
patrick brisbin <pbrisbin@gmail.com>
2018-05-23 15:26:43 -0400

    Move RepoWithStats to Model.Repo

--- src/Model/Repo.hs
+++ src/Model/Repo.hs
@@ -1,21 +1,25 @@
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE TypeFamilies #-}
 
 module Model.Repo
     ( findOrCreateRepo
+    , RepoWithStats(..)
+    , repoWithStats
     ) where
 
 import ClassyPrelude
 
 import Database.Persist.Sql
-import GitHub.Data hiding (Repo(..))
+import GitHub.Data hiding (Repo(..), User(..))
 import qualified GitHub.Data as GH
 import GitHub.Data.Apps
 import Model
 
 findOrCreateRepo :: GH.Repo -> Id Installation -> DB (Entity Repo)
 findOrCreateRepo ghRepo installationId = do
-    let repo = Repo
+    let
+        repo = Repo
             { repoOwner = simpleOwnerLogin $ GH.repoOwner ghRepo
             , repoName = GH.repoName ghRepo
             , repoInstallationId = installationId
@@ -23,7 +27,35 @@ findOrCreateRepo ghRepo installationId = do
             , repoDebugEnabled = False
             }
 
-    upsert repo
+    upsert
+        repo
         [ RepoInstallationId =. repoInstallationId repo
         , RepoIsPrivate =. repoIsPrivate repo
         ]
+
+data RepoWithStats = RepoWithStats
+    { rwsRepo :: Entity Repo
+    , rwsJobCount :: Int
+    , rwsErrorCount :: Int
+    , rwsLastJob :: Maybe (Entity Job)
+    }
+
+repoWithStats :: Entity Repo -> DB RepoWithStats
+repoWithStats repo =
+    RepoWithStats repo
+        <$> count
+                [ JobOwner ==. repoOwner (entityVal repo)
+                , JobRepo ==. repoName (entityVal repo)
+                ]
+        <*> count
+                [ JobOwner ==. repoOwner (entityVal repo)
+                , JobRepo ==. repoName (entityVal repo)
+                , JobExitCode !=. Just 0
+                , JobExitCode !=. Nothing
+                ]
+        <*> selectFirst
+                [ JobOwner ==. repoOwner (entityVal repo)
+                , JobRepo ==. repoName (entityVal repo)
+                , JobCompletedAt !=. Nothing
+                ]
+                [Desc JobCreatedAt]
