118b290fb9b3b1080bc1528253b7ecc9d94c193c
patrick brisbin <pbrisbin@gmail.com>
2018-06-04 09:13:46 -0400

    Document collaborators endpoint

--- src/Model/Collaborator.hs
+++ src/Model/Collaborator.hs
@@ -3,6 +3,10 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
 
+-- |
+--
+-- <https://developer.github.com/v3/repos/collaborators/#review-a-users-permission-level>
+--
 module Model.Collaborator
     ( collaboratorCanRead
     ) where
