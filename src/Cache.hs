0be65f93b91a3fe5dd5653aacb792a66c31a1a54
patrick brisbin <pbrisbin@gmail.com>
2018-08-31 10:02:21 -0400

    Refactor Repo Authorization
    - Make Cache a bit more useful
    - Extract and tighten up authorizeRepo
    - Make simple cases easier to spot, focus on important case

--- src/Cache.hs
+++ src/Cache.hs
@@ -1,13 +1,20 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Cache
     ( MonadCache(..)
     , CacheKey(..)
     , withCache
+    , caching
     ) where
 
 import Prelude
 
+import Control.Monad.Trans (lift)
 import Data.Aeson
 import Data.Text (Text)
+import qualified Data.Text as T
+import Database.Persist.Sql (SqlPersistT)
 
 newtype CacheKey = CacheKey Text
 
@@ -15,10 +22,14 @@ class Monad m => MonadCache m where
     getCache :: FromJSON a => CacheKey -> m (Maybe a)
     setCache :: ToJSON a => CacheKey -> a -> m ()
 
+instance MonadCache m => MonadCache (SqlPersistT m) where
+    getCache = lift . getCache
+    setCache k = lift . setCache k
+
 -- | Cache a value-producing action at the given key
 --
 -- The key will expire according to the @'setCache'@ implementation regardless
--- of how often its access. I.e. values aren't re-set after access.
+-- of how often its accessed. I.e. values aren't re-set after access.
 --
 withCache :: (MonadCache m, FromJSON a, ToJSON a) => CacheKey -> m a -> m a
 withCache key action = maybe produce pure =<< getCache key
@@ -26,3 +37,14 @@ withCache key action = maybe produce pure =<< getCache key
     produce = do
         val <- action
         val <$ setCache key val
+
+-- | A version of @'withCache'@ that takes a /key path/ of sorts
+--
+-- > caching ["foo", "bar"] f
+--
+-- is equivalent to
+--
+-- > withCache (CacheKey "foo.bar") f
+--
+caching :: (ToJSON a, FromJSON a, MonadCache m) => [Text] -> m a -> m a
+caching k = withCache (CacheKey $ T.intercalate "." k)
