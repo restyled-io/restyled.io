46068b812b3baa97d0de6fa8cf22bbf50d4fe050
patrick brisbin <pbrisbin@gmail.com>
2018-09-17 20:11:39 -0400

    Revert "Exchange for a valid GitLab access token"
    This reverts commit 52f6722626cee535533803f4e124135ca68e7f53.

--- src/SVCS/Names.hs
+++ src/SVCS/Names.hs
@@ -12,10 +12,8 @@ module SVCS.Names
     , mkPullRequestNum
     , GitHubUserId
     , GitHubUserName
-    , githubOwnerToUser
     , GitLabUserId
     , GitLabUserName
-    , gitlabOwnerToUser
     , RepoAccessToken(..)
     , RepoSVCS(..)
     ) where
@@ -46,15 +44,9 @@ mkPullRequestNum = mkId Proxy
 type GitHubUserId = Id User
 type GitHubUserName = Name User
 
-githubOwnerToUser :: OwnerName -> GitHubUserName
-githubOwnerToUser = mkName Proxy . untagName
-
 -- TODO: newtype for actual safety
 type GitLabUserId = Id User
 type GitLabUserName = Name User
-
-gitlabOwnerToUser :: OwnerName -> GitHubUserName
-gitlabOwnerToUser = mkName Proxy . untagName
 
 newtype RepoAccessToken = RepoAccessToken { unRepoAccessToken :: Text }
 
