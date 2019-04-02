5ab6abfa9866220c70b215e9cc9e20c344139ac0
patrick brisbin <pbrisbin@gmail.com>
2019-04-02 09:11:07 -0400

    Add admin/marketplace

---
+++ src/Handler/Admin/Marketplace.hs
@@ -0,0 +1,59 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TupleSections #-}
+
+module Handler.Admin.Marketplace
+    ( getAdminMarketplaceR
+    )
+where
+
+import Import
+
+import Backend.Marketplace (isPrivateRepoPlan)
+import Data.List (nub)
+import Yesod.Paginator
+import Yesod.Paginator.Instances ()
+
+data MarketplacePlanWithAccounts = MarketplacePlanWithAccounts
+    { mpwaPlan :: Entity MarketplacePlan
+    , mpwaAccounts :: [Entity MarketplaceAccount]
+    }
+
+getAdminMarketplaceR :: Handler Html
+getAdminMarketplaceR = do
+    (pages, noPlanRepoOwners) <- runDB $ do
+        plans <- selectPaginated 5 [] [Asc MarketplacePlanGithubId]
+        pages <- for plans $ \plan ->
+            MarketplacePlanWithAccounts plan <$> selectList
+                [MarketplaceAccountMarketplacePlan ==. entityKey plan]
+                [Asc MarketplaceAccountGithubId]
+
+        let
+            planOwners =
+                marketplacePlanWithAccountsOwners $ pageItems $ pagesCurrent
+                    pages
+
+        (pages, ) <$> fetchUniqueRepoOwnersExcept planOwners
+
+    adminLayout $ do
+        setTitle "Admin - Marketplace"
+        $(widgetFile "admin/marketplace")
+
+fetchUniqueRepoOwnersExcept
+    :: MonadIO m => [OwnerName] -> SqlPersistT m [OwnerName]
+fetchUniqueRepoOwnersExcept exceptOwners = do
+    owners <- repoOwner . entityVal <$$> selectList
+        [RepoOwner /<-. exceptOwners]
+        [Asc RepoOwner]
+    pure $ nub owners
+
+marketplacePlanWithAccountsOwners
+    :: [MarketplacePlanWithAccounts] -> [OwnerName]
+marketplacePlanWithAccountsOwners =
+    map (marketplaceAccountGithubOwner . entityVal) . concatMap mpwaAccounts
+
+marketplaceAccountGithubOwner :: MarketplaceAccount -> OwnerName
+marketplaceAccountGithubOwner = userToOwnerName . marketplaceAccountGithubLogin
+
+accountsList :: [OwnerName] -> Widget
+accountsList owners = $(widgetFile "admin/marketplace/accounts-list")
