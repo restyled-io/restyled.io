cd7b59ac8d9fac252d210c447d85166d0127e838
patrick brisbin <pbrisbin@gmail.com>
2019-05-17 09:18:56 -0400

    Get combinators from packages
    - Get fromMaybeM from extra
    - Get (<$$>) from composition-extra

--- src/Backend/Import.hs
+++ src/Backend/Import.hs
@@ -1,8 +1,6 @@
 module Backend.Import
     ( module X
-
-    , assertJust
-    , encodeStrict
+    , module Backend.Import
     )
 where
 
@@ -11,33 +9,35 @@ import RIO as X hiding (timeout)
 import Control.Error.Util as X (hush, hushT, note, noteT, (??))
 import Control.Monad.Except as X
     (ExceptT(..), liftEither, runExceptT, throwError, withExceptT)
+import Control.Monad.Extra as X (fromMaybeM)
 import Control.Monad.Trans.Maybe as X
 import Data.Aeson as X
 import Data.Aeson.Casing as X
 import Data.ByteString as X (ByteString)
+import Data.Functor.Syntax as X ((<$$>))
 import Data.Proxy as X
 import Data.Text as X (Text, pack, unpack)
 import Data.Time as X
 import Database.Persist as X
 import Database.Persist.Sql as X (SqlPersistT)
-import RIO.DB as X
-import RIO.Redis as X
-import System.Exit as X (ExitCode(..))
-import Web.PathPieces as X
-
--- * Lowest-level Yesod app modules we use
-import Import.NoFoundation as X (fromMaybeM, overEntity, (<$$>))
 import Model as X
 import Model.Job as X
 import Model.Repo as X
 import Model.RestyleMachine as X
 import Model.User as X
+import RIO.DB as X
+import RIO.Redis as X
 import Settings as X
+import System.Exit as X (ExitCode(..))
+import Web.PathPieces as X
 
 import Data.ByteString.Lazy (toStrict)
 
-assertJust :: (HasCallStack, MonadIO m) => String -> Maybe a -> m a
-assertJust msg = fromMaybeM (throwString $ "Failed assertion: " <> msg)
+fromJustNoteM :: MonadIO m => String -> Maybe a -> m a
+fromJustNoteM msg = fromMaybeM (throwString msg) . pure
 
 encodeStrict :: ToJSON a => a -> ByteString
 encodeStrict = toStrict . encode
+
+overEntity :: Entity a -> (a -> a) -> Entity a
+overEntity e f = e { entityVal = f $ entityVal e }
