b8e977a7e258625c44dba69c167bb3b33b1891e5
patrick brisbin <pbrisbin@gmail.com>
2019-06-27 11:00:59 -0400

    Add MaybeT persist helpers

--- src/Restyled/Models/User.hs
+++ src/Restyled/Models/User.hs
@@ -22,12 +22,11 @@ fetchMarketplacePlanForUser
 fetchMarketplacePlanForUser User {..} = runMaybeT $ do
     githubId <- hoistMaybe userGithubUserId
     githubLogin <- hoistMaybe userGithubUsername
-    account <- MaybeT $ getBy $ UniqueMarketplaceAccount githubId githubLogin
-    MaybeT $ get $ marketplaceAccountMarketplacePlan $ entityVal account
+    account <- getByT $ UniqueMarketplaceAccount githubId githubLogin
+    getT $ marketplaceAccountMarketplacePlan $ entityVal account
 
 fetchMarketplacePlanByLogin
     :: MonadIO m => GitHubUserName -> SqlPersistT m (Maybe MarketplacePlan)
 fetchMarketplacePlanByLogin username = runMaybeT $ do
-    account <- MaybeT
-        $ selectFirst [MarketplaceAccountGithubLogin ==. username] []
-    MaybeT $ get $ marketplaceAccountMarketplacePlan $ entityVal account
+    account <- selectFirstT [MarketplaceAccountGithubLogin ==. username] []
+    getT $ marketplaceAccountMarketplacePlan $ entityVal account
