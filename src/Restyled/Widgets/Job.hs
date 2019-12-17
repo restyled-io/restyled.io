20867f324f45ce22368eab2505714768d70da971
patrick brisbin <pbrisbin@gmail.com>
2019-12-17 15:26:38 -0500

    Add link to report an Issue with a Job
    Pre-populate a GitHub Issue with useful details about the Job.
    
    Closes #169.

--- src/Restyled/Widgets/Job.hs
+++ src/Restyled/Widgets/Job.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE TemplateHaskell #-}
 
 module Restyled.Widgets.Job
@@ -19,12 +20,15 @@ import Restyled.Prelude
 import qualified Data.Text as T
 import Formatting (format)
 import Formatting.Time (diff)
+import qualified Network.URI.Encode as URI
 import Restyled.Foundation
 import Restyled.Models
 import Restyled.Routes
 import Restyled.Settings
 import Restyled.Widgets.ContainsURLs
+import Restyled.Yesod
 import Text.Julius (RawJS(..))
+import Text.Shakespeare.Text (st)
 
 -- | Internal helper for rendering completion state
 data Completion
@@ -39,9 +43,44 @@ jobCompletion job = case (jobCompletedAt job, jobExitCode job) of
     _ -> InProgress
 
 jobCard :: (Entity Job, JobOutput) -> Widget
-jobCard (Entity jobId job, output) = do
+jobCard (jobE@(Entity jobId job), output) = do
     now <- liftIO getCurrentTime
+    issueUrl <- jobIssueURL jobE <$> getUrlRender
     $(widgetFile "widgets/job-card")
+
+jobIssueURL :: Entity Job -> (Route App -> Text) -> Text
+jobIssueURL (Entity jobId Job {..}) urlRender =
+    "https://github.com/restyled-io/restyler/issues/new"
+        <> "?title="
+        <> URI.encodeText title
+        <> "&body="
+        <> URI.encodeText body
+  where
+    title = "Problem with Restyler Job #" <> toPathPiece jobId
+    body = [st|
+Please use the below template to report an issue with this Job. **Don't submit
+sensitive information**, this is a public project. If you'd rather communicate
+privately, email support@restyled.io.
+
+---
+
+Hi there-
+
+I'm having a problem with a Restyled Job
+
+- **Restyled Job**: #{urlRender $ repoP jobOwner jobRepo $ jobR jobId}
+- **GitHub PR**: #{repoPullPath jobOwner jobRepo jobPullRequest}
+
+**What I expected to happen**:
+
+**What happened instead**:
+
+**Configuration** (if applicable):
+
+```yaml
+# .restyled.yaml as it was in the commit for the Job
+```
+|]
 
 jobOutput :: JobOutput -> Widget
 jobOutput output = $(widgetFile "widgets/job-output")
