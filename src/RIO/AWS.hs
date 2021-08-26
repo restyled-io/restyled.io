a00c829a7032703a1ae1ac083ab1091309288338
patrick brisbin <pbrisbin@gmail.com>
2021-08-25 21:27:25 -0400

    Remove unused definitions

--- src/RIO/AWS.hs
+++ src/RIO/AWS.hs
@@ -1,6 +1,5 @@
 module RIO.AWS
     ( HasAWS(..)
-    , runAWS
     , pageAWS
     , discoverAWS
     ) where
@@ -20,14 +19,6 @@ instance HasAWS AWS.Env where
 
 instance HasAWS env => HasAWS (HandlerData child env) where
     awsEnvL = handlerEnvL . siteL . awsEnvL
-
-runAWS
-    :: (MonadIO m, MonadReader env m, HasAWS env, AWS.AWSRequest a)
-    => a
-    -> m (AWS.Rs a)
-runAWS req = do
-    env <- view awsEnvL
-    liftIO $ runResourceT $ AWS.runAWST env $ AWS.send req
 
 pageAWS
     :: (MonadIO m, MonadReader env m, HasAWS env, AWS.AWSPager a)
--- src/Restyled/WebSockets.hs
+++ src/Restyled/WebSockets.hs
@@ -1,17 +1,5 @@
 module Restyled.WebSockets
-    (
-    -- * Helpers
-    -- | TODO: I think a since-fixed bug was causing the need for this
-    --
-    -- Try to write new code without it and see if it breaks. If not, remove it
-    -- where it is used currently.
-    --
-      ignoringConnectionException
-
-    -- * Wrappers
-    , sendTextDataAck
-
-    -- * Re-export
+    ( sendTextDataAck
     , module Yesod.WebSockets
     ) where
 
@@ -19,13 +7,6 @@ import Restyled.Prelude
 
 import qualified Network.WebSockets as WS
 import Yesod.WebSockets
-
-ignoringConnectionException
-    :: MonadUnliftIO m => WebSocketsT m () -> WebSocketsT m ()
-ignoringConnectionException = handle ignoreConnectionException
-
-ignoreConnectionException :: Applicative f => WS.ConnectionException -> f ()
-ignoreConnectionException _ = pure ()
 
 sendTextDataAck :: (MonadIO m, WS.WebSocketsData a) => a -> WebSocketsT m Text
 sendTextDataAck a = sendTextData a >> receiveData
