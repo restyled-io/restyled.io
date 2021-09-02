1f52d3b570e87ce2e8b95e9ea1d6d358bc2216b9
patrick brisbin <pbrisbin@gmail.com>
2021-09-02 12:08:32 -0400

    Experiment with new webhooks queue
    For some of our own repositories, enqueue to a new queue where the agent
    will be listening.

--- src/Restyled/Handlers/Webhooks.hs
+++ src/Restyled/Handlers/Webhooks.hs
@@ -5,7 +5,9 @@ module Restyled.Handlers.Webhooks
 import Restyled.Prelude
 
 import Conduit
+import Data.Aeson.Lens
 import Data.ByteString.Lazy (toStrict)
+import qualified Data.ByteString.Lazy as BSL
 import Data.Conduit.Binary
 import Restyled.Backend.Webhook
 import Restyled.Foundation
@@ -14,5 +16,18 @@ import Restyled.Yesod
 postWebhooksR :: Handler ()
 postWebhooksR = do
     body <- runConduit $ rawRequestBody .| sinkLbs
-    runRedis $ enqueueWebhook $ toStrict body
+    runRedis $ if enqueueToExperimentalRestyler body
+        then enqueueWebhookTo experimentalQueueName $ toStrict body
+        else enqueueWebhook $ toStrict body
     sendResponseStatus @_ @Text status201 ""
+
+enqueueToExperimentalRestyler :: BSL.ByteString -> Bool
+enqueueToExperimentalRestyler body = fromMaybe False $ do
+    name <- body ^? key "repository" . key "full_name" . _String
+    pure $ name `elem` experimentalRepositories
+
+experimentalRepositories :: [Text]
+experimentalRepositories = ["restyled-io/restyled.io", "restyled-io/agent"]
+
+experimentalQueueName :: ByteString
+experimentalQueueName = "restyled:agent:webhooks"
