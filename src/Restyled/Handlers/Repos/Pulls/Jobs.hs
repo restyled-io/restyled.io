2cade4ef47f2db3f06cc5341efd6d43e8bc73e87
patrick brisbin <pbrisbin@gmail.com>
2021-09-05 06:35:15 -0400

    Break up Handlers.Repos

---
+++ src/Restyled/Handlers/Repos/Pulls/Jobs.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+module Restyled.Handlers.Repos.Pulls.Jobs
+    ( getRepoPullJobsR
+    ) where
+
+import Restyled.Prelude
+
+import Restyled.Foundation
+import Restyled.Models
+import Restyled.Settings
+import Restyled.Widgets.Job
+import Restyled.Yesod
+
+getRepoPullJobsR :: OwnerName -> RepoName -> PullRequestNum -> Handler Html
+getRepoPullJobsR owner name num = do
+    pages <- runDB $ selectPaginated
+        5
+        [JobOwner ==. owner, JobRepo ==. name, JobPullRequest ==. num]
+        [Desc JobCreatedAt]
+
+    defaultLayout $ do
+        setTitle $ toHtml $ repoPullPath owner name num <> " jobs"
+        $(widgetFile "jobs")
