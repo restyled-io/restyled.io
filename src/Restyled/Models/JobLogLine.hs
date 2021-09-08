e5071df007cf07f2b13f0a1e9cf85737263f4d14
patrick brisbin <pbrisbin@gmail.com>
2021-09-08 15:25:28 -0400

    Fix deprecated streamJobLogLines
    Forgot to account for compressed jobLog, oops.

--- src/Restyled/Models/JobLogLine.hs
+++ src/Restyled/Models/JobLogLine.hs
@@ -1,12 +1,37 @@
 module Restyled.Models.JobLogLine
-    ( fetchJobLogLines
-    , streamJobLogLines
+    ( streamJobLogLines
+
+    -- * Exported only for deprecated 'completeJob'
+    , fetchJobLogLines
     ) where
 
 import Restyled.Prelude
 
 import Conduit
 import Restyled.Models.DB
+
+streamJobLogLines
+    :: (MonadUnliftIO m, MonadReader env m, HasSqlPool env)
+    => JobId
+    -> Maybe UTCTime
+    -> ConduitT () [JobLogLine] m ()
+streamJobLogLines jobId mSince = do
+    jobLogLines <- lift $ runDB $ do
+        mCompressed <- fetchCompressedJobLog jobId mSince
+        maybe (entityVal <$$> fetchJobLogLines jobId mSince) pure mCompressed
+
+    yield jobLogLines
+
+fetchCompressedJobLog
+    :: MonadIO m => JobId -> Maybe UTCTime -> SqlPersistT m (Maybe [JobLogLine])
+fetchCompressedJobLog jobId mSince = runMaybeT $ do
+    job <- getT jobId
+    jsonb <- hoistMaybe $ jobLog job
+    pure
+        $ maybe id (\s -> dropWhile ((<= s) . jobLogLineCreatedAt)) mSince
+        $ sortOn jobLogLineCreatedAt
+        $ map entityVal
+        $ unJSONB jsonb
 
 fetchJobLogLines
     :: MonadIO m => JobId -> Maybe UTCTime -> SqlPersistT m [Entity JobLogLine]
@@ -15,12 +40,3 @@ fetchJobLogLines jobId mSince = selectList
         [Just (JobLogLineJob ==. jobId), (JobLogLineCreatedAt >.) <$> mSince]
     )
     [Asc JobLogLineCreatedAt]
-
-streamJobLogLines
-    :: (MonadUnliftIO m, MonadReader env m, HasSqlPool env)
-    => JobId
-    -> Maybe UTCTime
-    -> ConduitT () [JobLogLine] m ()
-streamJobLogLines jobId mSince = do
-    jobLogLines <- lift $ runDB $ entityVal <$$> fetchJobLogLines jobId mSince
-    yield jobLogLines
