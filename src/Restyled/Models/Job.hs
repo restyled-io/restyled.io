0f63d3f2b103daf6dbb5e56f29f61204be50ea1b
patrick brisbin <pbrisbin@gmail.com>
2021-09-22 09:53:17 -0400

    Remove unused Model.Job functions

--- src/Restyled/Models/Job.hs
+++ src/Restyled/Models/Job.hs
@@ -1,34 +1,13 @@
 module Restyled.Models.Job
     (
-    -- * @'JobOutput'@
-      captureJobLogLine
-    , fetchLastJobLogLineCreatedAt
-
     -- * Temporary log-transition helpers
-    , markJobAsCloudWatch
+      markJobAsCloudWatch
     , jobIsCloudWatch
     ) where
 
 import Restyled.Prelude
 
 import Restyled.Models.DB
-
-captureJobLogLine :: MonadIO m => JobId -> Text -> Text -> SqlPersistT m ()
-captureJobLogLine jobId stream content = do
-    now <- liftIO getCurrentTime
-    insert_ JobLogLine
-        { jobLogLineJob = jobId
-        , jobLogLineCreatedAt = now
-        , jobLogLineStream = stream
-        , jobLogLineContent = content
-        }
-
-fetchLastJobLogLineCreatedAt
-    :: MonadIO m => JobId -> SqlPersistT m (Maybe UTCTime)
-fetchLastJobLogLineCreatedAt jobId =
-    jobLogLineCreatedAt . entityVal <$$> selectFirst
-        [JobLogLineJob ==. jobId]
-        [Desc JobLogLineCreatedAt]
 
 markJobAsCloudWatch :: Job -> Job
 markJobAsCloudWatch job = job { jobStdout = Just cwSigil }
