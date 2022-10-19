fe81cf5ae96f4bf46d06e10e81f2b1f3922aec4f
patrick brisbin <pbrisbin@gmail.com>
2022-10-19 09:44:56 -0400

    Update clone token scrubbing
    With the new format, our 58 characters is not correct. We did still
    fail-safe by over-scrubbing, but we can probably do better. This has
    turned out to be the only case of the token appearing in logs, so
    getting more specific will allow a better result.

--- src/Restyled/Scrub.hs
+++ src/Restyled/Scrub.hs
@@ -12,17 +12,15 @@ import qualified Data.Text as T
 -- will include the "x-access-token:...@github.com" secret. These are ephemeral
 -- and only valid for less than 5 minutes, but we shouldn't show them anyway.
 --
--- This function naively strips the 58 characters before any appearance of
--- "@github.com", which addresses known error messages and should fail-safe by
--- over-scrubbing when it gets something wrong.
+-- This function naively masks @x-access-token:{...}@github.com@, which
+-- addresses known error messages. We rely on the ephemeralness of tokens to
+-- address unknown messages.
 --
 scrubGitHubToken :: Text -> Text
 scrubGitHubToken msg = fromMaybe msg $ do
-    (before, after) <- breakOnDrop "@github.com" msg
-    pure
-        $ T.dropEnd 58 before
-        <> "<SCRUBBED>@github.com"
-        <> scrubGitHubToken after
+    (before, rest) <- breakOnDrop "x-access-token:" msg
+    (_, after) <- breakOnDrop "@github.com" rest
+    pure $ before <> "x-access-token:***@github.com" <> scrubGitHubToken after
 
 breakOnDrop :: Text -> Text -> Maybe (Text, Text)
 breakOnDrop needle = secondM (T.stripPrefix needle) . T.breakOn needle
