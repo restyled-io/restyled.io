31f81b9c95f4fb39e8949f4cc3d2254ed56229a2
patrick brisbin <pbrisbin@gmail.com>
2024-11-12 11:38:37 -0500

    Silently respond 200 to webhooks
    The next step on the way to not responding at all.

--- src/Restyled/Handlers/Webhooks.hs
+++ src/Restyled/Handlers/Webhooks.hs
@@ -4,23 +4,8 @@ module Restyled.Handlers.Webhooks
 
 import Restyled.Prelude
 
-import Conduit
-import Data.Conduit.Binary
-import Restyled.Disabled
 import Restyled.Foundation
-import Restyled.Queues
-import Restyled.Redis
 import Restyled.Yesod
 
 postWebhooksR :: Handler ()
-postWebhooksR = do
-  body <- runConduit $ rawRequestBody .| sinkLbs
-
-  emitDisabledStatus body >>= \case
-    Left {} -> do
-      -- Not emitted, proceed normally
-      qs <- view queuesL
-      runRedis $ enqueue qs $ toStrict body
-    Right {} -> pure ()
-
-  sendResponseStatus @_ @Text status201 ""
+postWebhooksR = sendResponseStatus @_ @Text status200 ""
